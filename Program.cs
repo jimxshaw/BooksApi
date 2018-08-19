f404e1955eb784eafe2f61f01f69692474e209f5
Jim Shaw <jimxshaw@gmail.com>
2018-08-19 16:45:11 -0500

    Add initial migration. Modify program to do initial data migration when the app runs.

--- Program.cs
+++ Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BooksApi.Contexts;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace BooksApi
@@ -14,7 +17,24 @@ namespace BooksApi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+ 
+            // Migrate the database. Best practice: In Main, using service scope.
+            using (IServiceScope scope = host.Services.CreateScope()) 
+            {
+                try 
+                {
+                    var context = scope.ServiceProvider.GetService<BooksContext>();
+                    context.Database.Migrate();
+                } 
+                catch (Exception ex) 
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
